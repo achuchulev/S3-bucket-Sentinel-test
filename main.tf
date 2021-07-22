c536e8d254cff45d2684b9bf276541a546a6aa4b
nikcbg <nikcbg@yahoo.com>
2021-07-22 15:15:51 +0300

    Update main.tf

--- main.tf
+++ main.tf
@@ -2,11 +2,6 @@ provider "aws" {
   region = "us-east-1"
 }
 
-resource "aws_kms_key" "mykey" {
-  description             = "KKMS key"
-  deletion_window_in_days = 10
-}
-
 resource "aws_s3_bucket" "test" {
   bucket = "nikolay-test-bucket"
   acl    = "private"
@@ -14,14 +9,6 @@ resource "aws_s3_bucket" "test" {
   tags = {
     Name        = "nikolay-test-bucket"
     Environment = "dev"
-  }
-  server_side_encryption_configuration {
-    rule {
-      apply_server_side_encryption_by_default {
-        kms_master_key_id = aws_kms_key.mykey.arn
-        sse_algorithm     = "aws:kms"
-      }
-    }
   }
 }
 
